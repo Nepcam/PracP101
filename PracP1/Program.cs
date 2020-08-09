9f57e4a3029d13401408111468bc3b5e87a82568
Nepcam <cameron.nepe@gmail.com>
2020-08-10 08:17:30 +1200

    updates

--- PracP1/Program.cs
+++ PracP1/Program.cs
@@ -1,50 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace PracP1
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    // Cameron Nepe
+    //ID: 1262199
+    public class Program
     {
-      int n = 0;
-      string s = "";
-
-      // Check if 2 argumnets
-      if (args.Length == 2) {
-        if (int.TryParse(args[0], out n)) {
-          // Successful parse, so use n
-          s = args[1]; // Second argument is character
-          // Draw a line of characters
-          DrawChars(n, s);
-        } else {
-          // Unsuccessful parse, so no n value
-        }
-      }
-
-      // Wait for user to have read the output
-      Console.WriteLine();
-      Console.Write("<Press enter to finish>");
-      Console.ReadLine();
-    }
-
-    /// <summary>
-    /// Draws a line of characters
-    /// </summary>
-    /// <param name="n">Number of characters to draw</param>
-    /// <param name="s">Character to draw n times</param>
-    static void DrawChars(int n, string s)
-    {
-        for (int j = 0; j < n; j++)
+        public static void Main(string[] args)
         {
-            for (int i = 0; i < n; i++)
+            int n = 0;
+            string s = "";
+
+            // Check if 2 argumnets
+            if (args.Length == 2)
             {
-                Console.Write(s);
+                if (int.TryParse(args[0], out n))
+                {
+                    // Successful parse, so use n
+                    s = args[1]; // Second argument is character
+                                 // Draw a line of characters
+                    DrawChars(n, s);
+                }
+                else
+                {
+                    // Unsuccessful parse, so no n value
+                }
             }
+
+            // Wait for user to have read the output
             Console.WriteLine();
+            Console.Write("<Press enter to finish>");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Draws a line of characters
+        /// </summary>
+        /// <param name="n">Number of characters to draw</param>
+        /// <param name="s">Character to draw n times</param>
+        static void DrawChars(int n, string s)
+        {
+            for (int j = 0; j < n; j++) // number of rows
+            {
+                for (int i = 0; i < n; i++) // number of columns
+                {
+                    Console.Write(s);
+                }
+                Console.WriteLine();
+            }
         }
     }
-
-  }
 }
